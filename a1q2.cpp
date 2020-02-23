c30b850f33bdf0b74081d050727354581b884f20
赵晨 <zhaochen@zhaochendeMacBook-Pro.local>
2020-02-22 20:06:52 -0500

    add part2 documentation

--- a1q2.cpp
+++ a1q2.cpp
@@ -1,82 +1,77 @@
 #include "wall.h"
 #include "disjointset.h"
 #include "permutation.h"
-//store the adjacent rooms number to room[0] room[1], 
-void find_adjacent_rooms( int room[2], int wall, int n ) {
+//according wall number and column number
+//determine the rooms and store the adjacent rooms number to room[0] room[1]
+void find_adjacent_rooms( int room[2], int wall, int col ) {
     // Determine which row and column
+    // row num range [0, row-1]
+    // col num range [0, col-1]
 	if(wall > 0) {
 		
-		int row = (wall - 1) / (2 * n - 1);    //range[0, row-1]
-		int column = (wall - 1) % (2 * n - 1);  //range[0, col-1]
+		int row = (wall - 1) / (2 * col - 1);
+		int column = (wall - 1) % (2 * col - 1);
 
 		// Determine which rooms they connect
-		if (column < n - 1) {   
+		if (column < col - 1) {
 			// A vertical wall
-			room[0] = n * row + column;
-			room[1] = n * row + column + 1;
+			room[0] = col * row + column;
+			room[1] = col * row + column + 1;
 		}
 		else {
 			// A horizontal wall
-			column += 1 - n;            
+            // adjust column number
+			column += 1 - col;
 			 
-			room[0] = n * row + column;   
-			room[1] = n * (row + 1) + column;   
+			room[0] = col * row + column;
+			room[1] = col * (row + 1) + column;
 		}
 	}  
 }
 
+// according to the row and col number, generate a maze and store the walls of the maze to walls array.function return the number of the remaining walls.
 int generateMaze(int row, int col, Wall walls[]) {
 	//get cells number, walls number
 	int cells_num = row * col;
-	int disjoint_sets_num = 0;
-	int walls_num = 2 * row * col - row - col; //2*4*4-8=24
+	int walls_num = 2 * row * col - row - col;
+    int disjoint_sets_num = 0;
 	int rc = 0;
 	int index = 0;
-	if (row == 1) {
-		 
-		for (int i = 0; i < walls_num; i++) {
-			walls[i].set(-1, -1);
-		}
-	}
-	else if (col == 1) {
-		for (int i = 0; i < walls_num; i++) {
-			walls[i].set(-1, -1);
-		}
-	}
-	else {
+    
+    // only when both row and col > 1, maze exist
+	if (row > 1 && col > 1 ) {
 
 		//makeset for each cell
 		DisjointSet rooms(cells_num);
 		for (int i = 0; i < cells_num && rooms.makeSet(i); i++) {
 			disjoint_sets_num++;
 		}
-		//create Permutation obj to store room
+		//create Permutation obj to store untested walls which could make sure each wall only can be selected randomly without duplicate
 		Permutation untested_walls(walls_num);
  
-		//randamly find two adjacent rooms 
+		//randamly find two adjacent rooms, if two rooms are not in same disjoint set, union them
+        //otherwise, store the wall between them to the next index of Wall array.
 		while (disjoint_sets_num > 1 ){
-			
+			//through wall, get two adjacent rooms number, store in room[0], room[1]
 			int wall = untested_walls.next();
- 
 			int room[2];
 			find_adjacent_rooms(room, wall, col);
 		 
-			// If the two rooms are not connected,
-			//   remove the wall between them and
-			//   take the union of the two sets.
+			// If the two rooms are not connected, take the union of the two room.
 			if (rooms.findSet(room[0]) != rooms.findSet(room[1])) {				 
 				rooms.unionSets(room[0], room[1]);
 				disjoint_sets_num--;
 				walls_num--;
 			}
-			else {				 		 
-			//	walls[index++].set(room[0], room[1]);
-                                walls[index].set(room[0], room[1]);
-                                if(index < walls_num) index++;
+            //otherwise, store the wall to the next index of Wall array
+			else {
+                walls[index].set(room[0], room[1]);
+                //prevent segmentation fault
+                if(index < walls_num) index++;
 			}
 
 		};
-		//check the all the rest walls, store the adjacent rooms numbers at the end of walls array
+		//check all the rest walls, store the adjacent rooms numbers at the end of walls array
 		while (index < walls_num) {
 			int wall = untested_walls.next();
 			int room[2];
--- permutation.h
+++ permutation.h
@@ -1,62 +1,52 @@
-#ifndef QUEUE_H
-#define QUEUE_H
+#ifndef PERMUTATION_H
+#define PERMUTATION_H
 #include<cstdlib>
 
 class Permutation
 {
-private:
-	int max;
-	int *bound;
-	int *array;
+    private:
+        int max;
+        int bound;
+        int *array;
 
-public:
-	Permutation(int);
-	~Permutation();
-	int next();
-	void reset();
+    public:
+        Permutation(int);
+        ~Permutation();
+        int next();
 };
-
+//default constructor
+//used to store max int number from 1 to max to an integer array.
+//bound is used to record the number of remining items in the array.
 Permutation::Permutation(int n) {
 	max = n;
-	bound = new int(n);
+	bound = n;
 	array = new int[n];
 	for (int i = 0; i < max; i++) {
 		array[i] = i+1;
 	}
-	//test
- 
 }
 
 Permutation::~Permutation() {
-	delete bound;
+	 
 	delete[] array;
 }
-
+//next() function randomly select an item in the integer array and return this item
+//store the last item to the selected item's original spot.
+//decrease remaining items number by one(--bound);
 int Permutation::next() {
-	if (*bound > 1) {
-		int index = std::rand() % (*bound);
-	//	std::cout << "index: " << index << "\t";
+	if (bound > 1) {
+		int index = std::rand() % bound;
 		int result = array[index];
 
-		--*bound;
-		array[index] = array[*bound];
-	//	std::cout << "result: " << result << "\t";
+		--bound;
+		array[index] = array[bound];
 		return result;
 
 	}
 	else {
 		return array[0];
-	//	std::cout << "test *bound = 1" <<array[0];
 	}
-		
+    
 }
 
-void Permutation::reset() {
-	for (int i = 0; i < max; ++i) {
-		array[i] = i;
-	}
-
-	*bound = max;
-}
- 
 #endif
